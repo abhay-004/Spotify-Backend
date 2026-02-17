62574779dcc605f1a33cbd8065b654e4d3bc30b0
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-02-17 19:21:23 +0530

    create auth routes

---
+++ src/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { registerUser } from "../controllers/auth.controller.js";
+
+const authRouter = express.Router();
+
+//routes
+
+authRouter.post("/register", registerUser);
+
+export default authRouter;
