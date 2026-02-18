8c2de1277473e08f6b05d6f2d4d70d0fc2ff5a80
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-02-18 21:26:13 +0530

    create storage

---
+++ src/services/storage.js
@@ -0,0 +1,17 @@
+import ImageKit from "@imagekit/nodejs";
+
+const ImageKitClient = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+const uploadFile = async (file) => {
+  const result = await ImageKitClient.files.upload({
+    file,
+    fileName: "music_" + Date.now(),
+    folder: "spotify/music",
+  });
+
+  return result;
+};
+
+export default uploadFile;
